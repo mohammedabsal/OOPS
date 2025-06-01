13edbb0e46aa54aac7eeba6c6f4225fd68587b74
mohammedabsal <mohammedabsal9943@gmail.com>
2025-06-01 13:46:50 +0530

    parameter understanding

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-class Student:
-    def __init__(self,name=None,number=None):
+class Student: ##class is the blueprint  methods+data member
+    def __init__(self,name=None,number=None): #self - refer to the current object itself // __init__ its automatically runs
         self.name=name
         self.number=number
     def family_detail(self,fathername,mothername):
         self.fathername=fathername
         self.mothername=mothername
-p1=Student()
-s1=Student("Absal",9943816710)
+s1=Student("Absal",99438) #we can directly pass the arguments to the class 
+p1=Student()  # we need  to create the object then pass the arguments 
 p1.family_detail("sk","sy")
 print(s1.name)
 print(p1.fathername)
